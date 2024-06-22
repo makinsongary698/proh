0637a370f997ceb70fc7454281cfb2e8361b6818
Gary <contact@solarencrypt.com>
2024-06-22 06:56:33 -0700

    Initial Commit

---
+++ Target Support Files/Pods-AIArtGenerator/Pods-AIArtGenerator-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AIArtGenerator : NSObject
+@end
+@implementation PodsDummy_Pods_AIArtGenerator
+@end
---
+++ Target Support Files/Pods-AIArtGeneratorTests/Pods-AIArtGeneratorTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AIArtGeneratorTests : NSObject
+@end
+@implementation PodsDummy_Pods_AIArtGeneratorTests
+@end
